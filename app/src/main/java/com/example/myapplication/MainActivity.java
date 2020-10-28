786880aa96f3f74022ed5bf3f5b1a2fe9e529f36
nykwon11 <a14a3@naver.com>
2020-10-28 16:50:19 +0900

    10월 28일 16:50 수정본

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.myapplication;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -19,10 +22,12 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout layout_2;
     LinearLayout layout_3;
     LinearLayout layout_4;
+    LinearLayout layout_bottom;
 
     private long backKeyPressedTime = 0;
     private Toast toast;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent); // 액티비티 이동 구문
             }
         });
+
         layout_2 = findViewById(R.id.layout_2);
 
         layout_2.setOnClickListener(new View.OnClickListener() {
@@ -59,8 +65,17 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        layout_4= findViewById(R.id.layout_4);
-        layout_4.setOnClickListener(new View.OnClickListener(){
+        layout_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //전화 걸기
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:119"));
+                startActivity(intent);
+            }
+        });
+
+        layout_bottom= findViewById(R.id.layout_bottom);
+        layout_bottom.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -1,17 +1,21 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 
 public class MainActivity2 extends AppCompatActivity {
 
     MediaPlayer player;
-
+    ConstraintLayout alarmscreen;
 
     @Override
     protected void onDestroy() {
@@ -27,26 +31,29 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        Button button1 = (Button) findViewById(R.id.button2);
-        Button button2 = (Button) findViewById(R.id.button3);
+        alarmscreen = (ConstraintLayout) findViewById(R.id.alarmscreen);
+        final int myRed = ContextCompat.getColor(this, R.color.myRed);
+        final Button button1 = (Button) findViewById(R.id.button2);
+        final Button button2 = (Button) findViewById(R.id.button3);
+
+        player = MediaPlayer.create(MainActivity2.this, R.raw.siren);
 
         button1.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                player = MediaPlayer.create(MainActivity2.this, R.raw.siren);
                 player.start();
+                alarmscreen.setBackgroundColor(myRed);
             }
         });
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                    player.stop();
+                if (player.isPlaying()) {
                     player.reset();
-
-
+                    alarmscreen.setBackgroundColor(Color.WHITE);
+                    player = MediaPlayer.create(MainActivity2.this, R.raw.siren);
+                }
             }
         });
     }
